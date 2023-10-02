685831e8ce7efc7befade993d0d7295e258f3e2d
Matias-Zahn <zahnmatias14@gmail.com>
2023-10-02 13:11:37 -0300

    loader finalizado

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -4,7 +4,20 @@ function Loader() {
       <div className="grid place-items-center gap-5">
         <img src="/icons-loaders/Cloud-loader.svg" alt="" />
         <img src="/icons-loaders/title-loader.svg" alt="" />
-        <img src="/icons-loaders/rectangule-loader.svg" alt="" />
+        <div className="relative flex">
+          <img
+            className=""
+            src="/icons-loaders/rectangule-loader.svg"
+            alt=""
+          />
+          <div>
+            <img
+              className="absolute left-0 bottom-[2px] ml-4"
+              src="/icons-loaders/icons-loaders.svg"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );

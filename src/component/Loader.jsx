0f28f0bc235ed774077d1dfebbdaaf9677cc56b3
Matias-Zahn <zahnmatias14@gmail.com>
2023-10-02 02:39:56 -0300

    separacion del loader

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="w-[100%] h-[100vh] grid place-items-center ">
-      <div className="grid place-items-center">
+      <div className="grid place-items-center gap-5">
         <img src="/icons-loaders/Cloud-loader.svg" alt="" />
         <img src="/icons-loaders/title-loader.svg" alt="" />
         <img src="/icons-loaders/rectangule-loader.svg" alt="" />
